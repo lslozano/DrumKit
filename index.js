d8f6ca246df2598c642277a5c59aa98a8cf32d16
Luis Lozano <lslozano11@gmail.com>
2020-07-01 21:05:17 -0500

    Use of Switch statement to play audio file
    Depending on the innerText of the button a specific audio file will be played thanks to the Switch statement.

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
 const buttons = document.querySelectorAll(".drum");
+const tom1 = new Audio("./sounds/tom-1.mp3");
+const tom2 = new Audio("./sounds/tom-2.mp3");
+const tom3 = new Audio("./sounds/tom-3.mp3");
+const tom4 = new Audio("./sounds/tom-4.mp3");
+const crash = new Audio("./sounds/crash.mp3");
+const kickBass = new Audio("./sounds/kick-bass.mp3");
+const snare = new Audio("./sounds/snare.mp3");
 
 function handleClick() {
-  const audio = new Audio("./sounds/tom-1.mp3");
-  audio.play();
+
+  const buttonText = this.innerText;
+
+  switch(buttonText) {
+    case "w":
+      tom1.play();
+      break;
+
+    case "a":
+      tom2.play();
+      break;
+
+    case "s":
+      tom3.play();
+      break;
+
+    case "d":
+      tom4.play();
+      break;
+
+    case "j":
+      crash.play();
+      break;
+
+    case "k":
+      kickBass.play();
+      break;
+
+    case "l":
+      snare.play()
+      break;
+
+    default:
+      console.log(buttonText);
+  }
 }
 
 buttons.forEach(button => button.addEventListener("click", handleClick));
 
 /*
+NOTES:
+
 There is another way to get all the buttons with a for loop.
 
 const numberOfDrumButtons = document.querySelectorAll(".drum").length;
